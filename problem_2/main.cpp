fd51a65e5d2a43ee7e1a2f89243b4898c8701390
Semenova-Zvenigorodskaya Sofia <sofia.ladypython@gmail.com>
2019-11-07 10:08:03 +0300

    Update main.cpp

--- problem_2/main.cpp
+++ problem_2/main.cpp
@@ -2,8 +2,32 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    /// input your code here
+    char ans;
+    do 
+    {
+        int n, m;
+        cin >> n >> m;
+
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 0; j <= n; ++j) {
+                if (i == 0 && j == 0)
+                    cout << '\t';
+                else if (i == 0)
+                    cout << j << '\t';
+                else if (j == 0)
+                    cout << i << '\t';
+                else
+                    cout << i * j % m << '\t';
+            }
+            cout << '\n';
+        }
+        
+        cout << "Do you want to rerun the program? y-yes, n-no: ";
+        cin >> ans;
+    } while (ans != 'n');
     return 0;
 }
